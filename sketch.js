f6b4ddb8849221e53836860edb7647a620290f3d
Saanvi Gupta <65902729+Saanvi2828@users.noreply.github.com>
2020-10-08 20:03:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var allPlayers;
+var hypnoticBall, database;
+var position;
+var gameState=0;
+var form,player,game,playerCount
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+game=new Game()
+game.getState()
+game.start()
+}
+
+function draw(){
+
+}
